bf82de95ab973ef35f482f9f15da2eda8a9389d5
Donny Layug <donn.layug@yahoo.com>
2019-04-29 16:31:10 +0800

    Finished developing the home page.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+// When the Home button is clicked
+
+const homeBtn = function() {
+    const home = document.querySelector('.home-btn');
+    const leftMenu = document.querySelector('.left-menu');
+    const rightMenu = document.querySelector('.right-menu');
+
+    home.addEventListener('click', function(e) {
+        e.preventDefault();
+        leftMenu.classList.toggle('open');
+        rightMenu.classList.toggle('open');
+    });
+
+    window.addEventListener('click', function (e) {
+
+        if (e.target !== home && e.target.parentNode !== home) {
+            leftMenu.classList.remove('open');
+            rightMenu.classList.remove('open');
+        }
+    });
+}
+homeBtn();
+
